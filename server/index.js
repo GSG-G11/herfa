acb16adb85de0d72a3af2380b0b94fa9493c028f
AbdallahGot <abdallah897107@gmail.com>
2022-04-19 16:19:32 +0300

    feat: add install and heroku-postbuild script relates #3

--- server/index.js
+++ server/index.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const port = app.get('port');
 
 app.listen(port, () => {
-  console.log(`server running at ${port}`);
+  console.log(`server running at http://localhost:${port}`);
 });
