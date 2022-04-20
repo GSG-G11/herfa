fe006b20ed27b3ebb728e1bcd226589860fbde60
unknown <stm1998@hotmail.com>
2022-04-20 16:35:25 +0300

    rename congi.sql to be config.sql and remove commit and begin from config file

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 
 const compression = require('compression');
 const router = require('./routes/index');
+const { serverError, clientError } = require('./controllers/errors');
 
 const app = express();
 app.set('port', process.env.PORT || 3030);
@@ -21,5 +22,8 @@ app.use('/api/v1', router);
 app.get('*', (req, res) => {
   res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
 });
+
+app.use(clientError);
+app.use(serverError);
 
 module.exports = app;
