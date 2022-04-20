301355bdb38fc0b7a0fe846b2ab453e9f879a6fe
Rand <randsohail98@gmail.com>
2022-04-21 02:36:52 +0300

    feat: create crafts Craftsman models

--- server/database/models/index.js
+++ server/database/models/index.js
@@ -1,7 +1,13 @@
+const { CraftsCraftsman } = require('./craftsCraftsman');
+const { MainServices } = require('./mainServices');
+const { SubServices } = require('./subServices');
 const { User } = require('./user');
 const { Work } = require('./work');
 
 module.exports = {
   User,
   Work,
+  MainServices,
+  SubServices,
+  CraftsCraftsman,
 };
--- server/database/models/subServices.js
+++ server/database/models/subServices.js
@@ -1,7 +1,6 @@
 const { Sequelize } = require('sequelize');
 const { sequelize } = require('../config');
 const { MainServices } = require('./mainServices');
-// const { User } = require('./user');
 
 const SubServices = sequelize.define('services', {
   id: {
@@ -13,20 +12,9 @@ const SubServices = sequelize.define('services', {
     type: Sequelize.STRING,
     allowNull: false,
   },
-  // main_service_id: {
-  //   type: Sequelize.INTEGER,
-  //   allowNull: false,
-  //   references: {
-  //     model: 'main_services',
-  //     key: 'id',
-  //   },
-  // },
 });
 
 MainServices.hasMany(SubServices);
 SubServices.belongsTo(MainServices);
-
-// SubServices.belongsToMany(User);
-// User.belongsToMany(SubServices);
 
 module.exports = { SubServices };
