a246b7921eeb146040f3f75d4a432ea12f5bee35
abdulrahman-2020 <akalousa@students.iugaza.edu.ps>
2022-04-23 10:34:43 +0300

    feat: create userinfocard component -create a card using antd -add icons using fontawesome-react library

--- client/src/Pages/Profile.tsx
+++ client/src/Pages/Profile.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import UserInfoCard from '../components/profile/UserInfoCard';
 
 function Profile() {
   return (
     <div className="container">
       {useTranslation().t('profile-greeting')}
+      <UserInfoCard />
     </div>
   );
 }
