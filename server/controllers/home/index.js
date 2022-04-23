df413b5a8639657450aadc939a84d196910fd6fb
Rand <randsohail98@gmail.com>
2022-04-23 10:16:10 +0300

    feat: make 5 test check all the data should received in /api/v1/home route

--- server/controllers/home/index.js
+++ server/controllers/home/index.js
@@ -1,15 +1,21 @@
 const { Location, Review, MainServices } = require('../../database/models');
+const { customError } = require('../errors');
 
-const Home = async (request, response) => {
-  const locationFromDB = await Location.findAll();
-  const mainServices = await MainServices.findAll();
-  const reviewFromDB = await Review.findAll({
-    limit: 10,
-    where: {
-      rate: 5,
-    },
-  });
-  response.json({ location: locationFromDB, topTenReviews: reviewFromDB, services: mainServices });
+const Home = async (request, response, next) => {
+  try {
+    const locationFromDB = await Location.findAll();
+    const mainServices = await MainServices.findAll();
+    const reviewFromDB = await Review.findAll({
+      limit: 10,
+      where: {
+        rate: 5,
+      },
+    });
+    // eslint-disable-next-line max-len
+    response.json({ location: locationFromDB, topTenReviews: reviewFromDB, services: mainServices });
+  } catch (error) {
+    next(customError('Database error', 502));
+  }
 };
 
 module.exports = { Home };
---
+++ server/test/homeRoute.test.js
@@ -0,0 +1,75 @@
+require('env2')('.env');
+const request = require('supertest');
+const app = require('../app');
+
+describe('Test GET /home', () => {
+
+  test('Get /api/v1/home return with status code equal 200 ', (done) => {
+    request(app)
+      .get('/api/v1/home')
+      .expect(200)
+      .end((error, response) => {
+        if (error) done(error)
+        expect(response.statusCode).toBe(200);
+        done();
+      })
+  });
+
+  test('Get /api/v1/home return a json file and 3 result as body', (done) => {
+    request(app)
+      .get('/api/v1/home')
+      .expect(200)
+      .expect('content-type', 'application/json; charset=utf-8')
+      .end((error, response) => {
+        if (error) done(error)
+        expect(response.type).toBe('application/json');
+        expect(response.body.hasOwnProperty('location')).toBe(true);
+        expect(response.body.hasOwnProperty('services')).toBe(true);
+        expect(response.body.hasOwnProperty('topTenReviews')).toBe(true);
+        done();
+      })
+  });
+
+  test('Get /api/v1/home return all the location data => length location equal 12', (done) => {
+    request(app)
+      .get('/api/v1/home')
+      .expect(200)
+      .expect('content-type', 'application/json; charset=utf-8')
+      .end((error, response) => {
+        if (error) done(error)
+        expect(response.body.location.length).toBe(12);
+        done();
+      })
+  });
+  test('Get /api/v1/home return all the top 10 reviews data => rate = 5 and length >= 10', (done) => {
+    request(app)
+      .get('/api/v1/home')
+      .expect(200)
+      .expect('content-type', 'application/json; charset=utf-8')
+      .end((error, response) => {
+        if (error) done(error)
+        expect(response.body.topTenReviews.length).toBeLessThan(11);
+        expect(response.body.topTenReviews).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({ rate: 5 }),
+          ])
+        );
+        done();
+      })
+  });
+
+  test('Get /api/v1/home return all the main services data => length 10 ', (done) => {
+    request(app)
+      .get('/api/v1/home')
+      .expect(200)
+      .expect('content-type', 'application/json; charset=utf-8')
+      .end((error, response) => {
+        if (error) done(error)
+        expect(response.body.services.length).toBe(10);
+        expect(response.body.services[0].name).toBe('الحدادة');
+        done();
+      })
+  });
+
+});
+
