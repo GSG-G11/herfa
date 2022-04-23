c3e70f8ae5a5298888be635470a52ecf88c6f0fc
Rand <randsohail98@gmail.com>
2022-04-23 11:04:50 +0300

    fix: un comment line in test.test.js file

--- server/test/test.test.js
+++ server/test/test.test.js
@@ -1,5 +1,5 @@
 require('env2')('.env');
-// console.log(process.env.TEST_DB_URL.split(5)[0])
+console.log(process.env.TEST_DB_URL.split(5)[0])
 test('should first', () => {
   expect(1).toBe(1);
 });
