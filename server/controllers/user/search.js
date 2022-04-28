d86e8a590bd6f514c28082fe699e615f53ac8f8a
unknown <stm1998@hotmail.com>
2022-04-28 16:42:24 +0300

    feat: search for provider by name or location

--- server/controllers/user/search.js
+++ server/controllers/user/search.js
@@ -1,32 +1,99 @@
-const { fn, col } = require('sequelize');
+const { Op, fn, col } = require('sequelize');
 
-const { User, Review, Location } = require('../../database/models');
-// const { searchRequestValidation } = require('../../utils/validation');
-// const { customError } = require('../errors');
+const { searchRequestValidation } = require('../../utils/validation');
+const { customError } = require('../errors');
+// --------------------- user nested include --------------------- //
+// --------------------- internal server error ------------------- //
+// locationId must be in group by clause ------------------------- //
+// const users = await Review.findAll({
+// attributes: [[fn('AVG', col('reviews.rate')), 'avgRating']],
+//   include: [
+//     {
+//       model: User,
+//       include: [
+//         {
+//           model: Location,
+//           where: {
+//             id: 1,
+//           },
+//         },
+//       ],
+//     },
+//   ],
+// });
+//     return res.status(200).json({
+//       success: true,
+//       users,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     return next(error);
+//   }
+// };
+
+const {
+  Location,
+  SubServices,
+  User,
+  Review,
+} = require('../../database/models');
 
 const getUsersWithReviews = async (req, res, next) => {
   try {
-    const users = await Review.findAll({
-      attributes: [[fn('AVG', col('reviews.rate')), 'avgRating']],
-      include: {
-        model: User,
-        where: { first_name: 'صباح' },
-        include: {
-          model: Location,
-          where: {
-            id: 1,
+    const { name, location } = await searchRequestValidation.validateAsync(
+      req.query,
+    );
+    console.log(req.query);
+    const users = await User.findAll({
+      where: {
+        [Op.or]: [
+          {
+            first_name: {
+              [Op.iLike]: `%${name}%`,
+            },
           },
-        },
+          {
+            last_name: {
+              [Op.iLike]: `%${name}%`,
+            },
+          },
+          {
+            locationId: location,
+          },
+        ],
       },
-      group: ['reviews.userId', 'user.id'],
+      include: [{ model: Location }, { model: SubServices }],
+      attributes: {
+        exclude: ['password', 'createdAt', 'updatedAt'],
+      },
     });
-    return res.status(200).json({
-      success: true,
-      users,
-    });
+    if (!users) {
+      throw customError('user does not exist', 400);
+    }
+    const reviews = await Promise.all(
+      users.map(async (user) => {
+        const reviewAvg = await Review.findAll({
+          where: { userId: user.id },
+          attributes: [[fn('AVG', col('reviews.rate')), 'avgRating']],
+          raw: true,
+          group: ['reviews.userId'],
+        });
+        return reviewAvg;
+      }),
+    );
+    const usersWithReviews = users.map((user, index) => ({
+      ...user.dataValues,
+      avgRating: reviews[index][0].avgRating,
+    }));
+
+    res.status(200).json({ msg: 'Search result', data: usersWithReviews });
   } catch (error) {
+    if (error.name === 'ValidationError') {
+      console.log(error);
+      return next(customError(error.message, 400));
+    }
     console.log(error);
-    return next(error);
+    next(error);
   }
 };
 
