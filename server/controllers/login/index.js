b2f79a77581889ddd62d29d1cc0c2de6ac8a70e3
AbdallahGot <abdallah897107@gmail.com>
2022-05-05 02:53:45 +0300

    test: test login route

--- server/controllers/login/index.js
+++ server/controllers/login/index.js
@@ -7,12 +7,12 @@ const loginHandler = async (req, res, next) => {
   try {
     const validateValue = await loginValidation(req.body);
     const checkedEmail = await getEmail(validateValue.email);
-    if (!checkedEmail) throw customError('incorrect password or email ...', 400);
+    if (!checkedEmail) throw customError('incorrect password or email ...', 401);
     const checkedPassword = await compare(validateValue.password, checkedEmail.password);
-    if (!checkedPassword) throw customError('incorrect password or email ...', 400);
+    if (!checkedPassword) throw customError('incorrect password or email ...', 401);
     res.json({ msg: 'logged in successfully ..', data: { id: checkedEmail.id } });
   } catch (err) {
-    if (err.name === 'ValidationError') next(customError(err.message, 400));
+    if (err.name === 'ValidationError') next(customError(err.message, 401));
     return next(err);
   }
 };
---
+++ server/test/login.test.js
@@ -0,0 +1,60 @@
+const request = require('supertest');
+const app = require('../app');
+const { build } = require('../database/seeders');
+const { sequelize } = require('../database/config');
+
+beforeAll(() => build());
+
+describe('Test POST api/v1/login', () => {
+
+  test('Post /api/v1/login return with status code equal 200 ', (done) => {
+    request(app)
+      .post('/api/v1/login')
+      .expect(200)
+      .send({email: 'abdallah897107@gmail.com', password: '12345678'})
+      .end((error, response) => {
+        if (error) done(error);
+        expect(response.statusCode).toBe(200);
+        expect(response.type).toBe('application/json');
+        done();
+      })
+  });
+  test('Post /api/v1/login return with status code equal 401 with invalid eamil ', (done) => {
+    request(app)
+      .post('/api/v1/login')
+      .expect(401)
+      .send({email: 'abdallah897107gmail.com', password: '12345678'})
+      .end((error, response) => {
+        if (error) done(error);
+        expect(response.statusCode).toBe(401);
+        expect(response.body.msg).toBe('\"email\" must be a valid email');
+        done();
+      })
+  });
+  test('Post /api/v1/login return with status code equal 401 with invalid password ', (done) => {
+    request(app)
+      .post('/api/v1/login')
+      .expect(401)
+      .send({email: 'abdallah897107@gmail.com', password: '1234'})
+      .end((error, response) => {
+        if (error) done(error);
+        expect(response.statusCode).toBe(401);
+        expect(response.body.msg).toBe("\"password\" with value \"1234\" fails to match the required pattern: /^[a-zA-Z0-9]{8,30}$/");
+        done();
+      })
+  });
+  test('Post /api/v1/login return with status code equal 401 with incorrect email or password ', (done) => {
+    request(app)
+      .post('/api/v1/login')
+      .expect(401)
+      .send({email: 'abdallah89710@gmail.com', password: '123456789'})
+      .end((error, response) => {
+        if (error) done(error);
+        expect(response.statusCode).toBe(401);
+        expect(response.body.msg).toBe("incorrect password or email ...");
+        done();
+      })
+  });
+})
+
+afterAll(() => sequelize.close());
