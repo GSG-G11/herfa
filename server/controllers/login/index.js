01db1a4703530e00106db941bca2b087a2413818
AbdallahGot <abdallah897107@gmail.com>
2022-05-05 14:08:20 +0300

    feat: add signToken and use it to encode the userToken and send it in cookie

--- server/controllers/login/index.js
+++ server/controllers/login/index.js
@@ -1,5 +1,6 @@
 const { compare } = require('bcrypt');
 const { getEmail } = require('../../database/models');
+const signToken = require('../../utils/signToken');
 const { loginValidation } = require('../../utils/validation');
 const { customError } = require('../errors');
 
@@ -10,6 +11,8 @@ const loginHandler = async (req, res, next) => {
     if (!checkedEmail) throw customError('incorrect password or email ...', 401);
     const checkedPassword = await compare(validateValue.password, checkedEmail.password);
     if (!checkedPassword) throw customError('incorrect password or email ...', 401);
+    const signTokenCookie = await signToken(checkedEmail.id);
+    if (signTokenCookie) res.cookie('userToken', signTokenCookie);
     res.json({ msg: 'logged in successfully ..', data: { id: checkedEmail.id } });
   } catch (err) {
     if (err.name === 'ValidationError') next(customError(err.message, 401));
