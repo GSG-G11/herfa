ce2f165a408b883e18f6db891d7ee6a8f5a55941
AbdallahGot <abdallah897107@gmail.com>
2022-05-05 14:17:44 +0300

    fix: delete console.log

--- server/utils/signToken.js
+++ server/utils/signToken.js
@@ -3,10 +3,8 @@ const { sign } = require('jsonwebtoken');
 module.exports = (userId) => new Promise((resolve, reject) => {
   sign({ providerID: userId, role: 'provider' }, process.env.SECRET_KEY, (err, token) => {
     if (err) {
-      // console.log(err, 'eroooooooor');
       return reject(err);
     }
-    // console.log(token, 'tammmmmmmmmmmam');
     return resolve(token);
   });
 });
