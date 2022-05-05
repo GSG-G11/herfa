4014a7b0e5024c365c8615b4e7d6720ab870b4ad
unknown <stm1998@hotmail.com>
2022-05-05 15:12:35 +0300

    refactor: refactor search to search by service and sub service

--- server/controllers/user/search.js
+++ server/controllers/user/search.js
@@ -2,76 +2,57 @@ const { Op, fn, col } = require('sequelize');
 
 const { searchRequestValidation } = require('../../utils/validation');
 const { customError } = require('../errors');
-// --------------------- user nested include --------------------- //
-// --------------------- internal server error ------------------- //
-// locationId must be in group by clause ------------------------- //
-// const users = await Review.findAll({
-// attributes: [[fn('AVG', col('reviews.rate')), 'avgRating']],
-//   include: [
-//     {
-//       model: User,
-//       include: [
-//         {
-//           model: Location,
-//           where: {
-//             id: 1,
-//           },
-//         },
-//       ],
-//     },
-//   ],
-// });
-//     return res.status(200).json({
-//       success: true,
-//       users,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     return next(error);
-//   }
-// };
 
 const {
   Location,
-  SubServices,
   User,
+  SubServices,
   Review,
+  MainServices,
 } = require('../../database/models');
 
 const getSearchResult = async (req, res, next) => {
   try {
-    const { name, location } = await searchRequestValidation.validateAsync(
-      req.query,
-    );
-    console.log(req.query);
-    const users = await User.findAll({
-      where: {
-        [Op.or]: [
-          {
-            first_name: {
-              [Op.iLike]: `%${name}%`,
-            },
+    const {
+      name, location, service, subservice, page,
+    } = await searchRequestValidation.validateAsync(req.query);
+    const where = {};
+    if (name) {
+      where[Op.or] = [
+        {
+          first_name: {
+            [Op.iLike]: `%${name}%`,
           },
-          {
-            last_name: {
-              [Op.iLike]: `%${name}%`,
-            },
+        },
+        {
+          last_name: {
+            [Op.iLike]: `%${name}%`,
           },
-          {
-            locationId: location,
-          },
-        ],
-      },
-      include: [{ model: Location }, { model: SubServices }],
-      attributes: {
-        exclude: ['password', 'createdAt', 'updatedAt'],
-      },
-    });
-    if (!users) {
-      throw customError('user does not exist', 400);
+        },
+      ];
     }
+    if (location) {
+      where.locationId = location;
+    }
+    if (subservice) {
+      const subServiceArray = subservice?.split(',');
+      where['$services.id$'] = subServiceArray;
+    }
+    if (service) {
+      where.mainServiceId = +service;
+    }
+    const { count, rows } = await User.findAndCountAll({
+      limit: 6,
+      offset: (page - 1) * 6,
+      where,
+      include: [
+        { model: Location },
+        { model: SubServices },
+        { model: MainServices },
+      ],
+    });
     const reviews = await Promise.all(
-      users.map(async (user) => {
+      rows.map(async (user) => {
         const reviewAvg = await Review.findAll({
           where: { userId: user.id },
           attributes: [[fn('AVG', col('reviews.rate')), 'avgRating']],
@@ -81,19 +62,22 @@ const getSearchResult = async (req, res, next) => {
         return reviewAvg;
       }),
     );
-    const usersWithReviews = users.map((user, index) => ({
+    const usersWithReviews = rows.map((user, index) => ({
       ...user.dataValues,
       avgRating: reviews[index][0].avgRating,
     }));
-
-    res.status(200).json({ msg: 'Search result', data: usersWithReviews });
+    return res.status(200).json({
+      msg: 'search result',
+      count,
+      data: usersWithReviews,
+    });
   } catch (error) {
-    if (error.name === 'ValidationError') {
-      console.log(error);
-      return next(customError(error.message, 400));
-    }
     console.log(error);
-    next(error);
+    if (error.name === 'ValidationError') {
+      next(customError(error.message, 400));
+    } else {
+      next(error);
+    }
   }
 };
 
