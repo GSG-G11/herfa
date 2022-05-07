50aef2b108ce1de0f8b7d341009674686a9dd4e3
abdulrahman-2020 <akalousa@students.iugaza.edu.ps>
2022-05-07 18:10:09 +0300

    refactor: use userId from work

--- server/controllers/work/deleteWork.js
+++ server/controllers/work/deleteWork.js
@@ -1,23 +1,20 @@
-const { Work, User } = require('../../database/models');
+const { Work } = require('../../database/models');
 const { customError } = require('../errors');
 const { paramsValidation } = require('../../utils/validation');
 
 const deleteWork = async (req, res, next) => {
   try {
-    const { userId } = req.body;
-    const user = await User.findByPk(userId);
+    const { id } = await paramsValidation.validateAsync(req.params);
     const { providerID } = req;
-    if (user.id !== +providerID) {
-      throw customError('Unauthorized', 401);
-    } else {
-      const { id } = await paramsValidation.validateAsync(req.params);
-      const work = await Work.findByPk(id);
-      if (!work) {
-        throw customError('work does not exist', 400);
-      }
-      await work.destroy({ where: { id } });
-      res.json({ msg: 'work deleted successfully' });
+    const work = await Work.findByPk(id);
+    if (!work) {
+      throw customError('work does not exist', 400);
     }
+    if (work.userId !== +providerID) {
+      throw customError('Unauthorized', 401);
+    }
+    await work.destroy({ where: { id } });
+    res.json({ msg: 'work deleted successfully' });
   } catch (error) {
     if (error.name === 'ValidationError') {
       return next(customError(error.message, 400));
