6c4e5d5f191eb2cce97ac0cd2534ccfd7fcbcacd
abdulrahman-2020 <akalousa@students.iugaza.edu.ps>
2022-05-07 19:34:55 +0300

    feat: create update controller Relates #77

--- server/test/work.test.js
+++ server/test/work.test.js
@@ -33,53 +33,11 @@ describe('Routes Tests GET /api/v1/work/:providerId', () => {
       .set('Cookie', [`token= ${process.env.TOKEN}`])
       .expect(200)
       .end((err, res) => {
-        console.log(res)
         if (err) done(err);
         expect(res.statusCode).toBe(200);
         done();
       });
   });
-
-  // test('Testing for delete work /api/v1/work/1 and get status 200', (done) => {
-  //   supertest(app)
-  //     .delete('/api/v1/work/10')
-  //     .set('Cookie', [`token= ${process.env.TOKEN}`])
-  //     .expect(200)
-  //     .end((err, res) => {
-  //       console.log(res)
-  //       if (err) done(err);
-  //       expect(res.statusCode).toBe(200);
-  //       done();
-  //     });
-  // });  
-
-  // test('Testing for delete work /api/v1/work/1 and get status 401 UnAuthorized', (done) => {
-  //   supertest(app)
-  //     .delete('/api/v1/work/1')
-  //     .set('Cookie', [`token= ${process.env.TOKEN}`])
-  //     .expect(401)
-  //     .end((err, res) => {
-  //       if (err) done(err);
-  //       expect(res.statusCode).toBe(401);
-  //       done();
-  //     });
-  // });
-
- 
-
-  // test('Testing for delete work /api/v1/work/1000 and get status 400 work not exist', (done) => {
-  //   supertest(app)
-  //     .delete('/api/v1/work/1000')
-  //     .set('Cookie', [`token= ${process.env.TOKEN}`])
-  //     .expect(400)
-  //     .end((err, res) => {
-  //       if (err) done(err);
-  //       expect(res.statusCode).toBe(400);
-  //       done();
-  //     });
-  // });
-
- 
 
   test('Testing for delete work /api/v1/work/1 and get status 401 UnAuthorized', (done) => {
     supertest(app)
