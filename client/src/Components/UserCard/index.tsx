0515426fdd39b3dbf26f5bd9aac36c9ee4ffae49
unknown <stm1998@hotmail.com>
2022-05-09 00:01:37 +0300

    refactor: reade user information from props and render it dynamicly

--- client/src/Components/UserCard/index.tsx
+++ client/src/Components/UserCard/index.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import { Card, Image, Rate } from 'antd';
 import { useTranslation } from 'react-i18next';
 import './style.css';
-import { OneUserType } from '../../utils';
+import { UserData } from '../../utils';
 
-function UserCard({ user }: { user: OneUserType }) {
+function UserCard({ user }: { user: UserData }) {
+  const {
+    first_name: firstName,
+    last_name: lastName,
+    email,
+    phone,
+    whatsapp,
+    description,
+    image,
+    avgRating,
+    main_service: mainServices,
+  } = user;
   const { t } = useTranslation();
   return (
     <Card bordered={false}>
@@ -13,27 +24,27 @@ function UserCard({ user }: { user: OneUserType }) {
           <Image
             width={170}
             height={180}
-            src={user.image}
+            src={image}
           />
         </div>
         <div className="search-user-card-body">
           <div className="search-user-card-info">
             <div className="div-80-2 search-user-name">
-              <h2>{`${user.firstName} ${user.lastName}`}</h2>
-              <Rate allowHalf defaultValue={5} disabled className="rate" />
+              <h2>{`${firstName} ${lastName}`}</h2>
+              <Rate allowHalf defaultValue={avgRating} disabled className="rate" />
             </div>
             <p>gaza</p>
             <div className="div-80-2 search-user-card-contact">
-              <p>{user.email}</p>
-              <p>{user.phone}</p>
+              <p>{email}</p>
+              <p>{phone}</p>
             </div>
             {t('servicesOffer')}
             <div className="div-80-2 search-user-card-contact">
-              <p>{user.email}</p>
-              <p>{user.phone}</p>
+              <p>{mainServices.name}</p>
             </div>
             <p className="search-user-card-services" />
-            <p>{user.description}</p>
+            <p>{description}</p>
+            <p>{whatsapp}</p>
           </div>
         </div>
       </div>
