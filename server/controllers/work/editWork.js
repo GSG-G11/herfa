d245943619f7c81bc18dfc18323c7cab16583d25
abdulrahman-2020 <akalousa@students.iugaza.edu.ps>
2022-05-10 14:44:55 +0300

    fix: request change

--- server/controllers/work/editWork.js
+++ server/controllers/work/editWork.js
@@ -6,8 +6,8 @@ const { uploadImage, deleteImage } = require('../../utils');
 const editWork = async (req, res, next) => {
   try {
     const {
-      id, title, content, image,
-    } = await workEditValidation.validateAsync(req.body);
+      id, title, content,
+    } = await workEditValidation.validateAsync({ ...req.body, workImg: req.files.workImg });
     const { providerID } = req;
     const work = await Work.findByPk(id);
     if (!work) {
@@ -16,26 +16,26 @@ const editWork = async (req, res, next) => {
     if (work.userId !== +providerID) {
       throw customError('Unauthorized', 401);
     }
-    let imgUrl = image;
-    const imgArr = image.split('/').pop();
-    const key = `${work.userId}/${imgArr}.png`;
+    let imgUrl = work.image;
 
-    const { imageAws } = req.files;
-    if (imageAws) {
-      const imgAws = await uploadImage(imageAws, work.userId);
+    const { workImg } = req.files;
+    if (workImg) {
+      const imgAws = await uploadImage(workImg, work.userId);
       imgUrl = imgAws.Location;
-      // console.log();
-      deleteImage(key);
     }
-
     await work.update({
+      where: { id: work.id },
       title,
       content,
       image: imgUrl,
     });
     res.json({ msg: 'work updated successfully', data: work });
+
+    if (workImg) {
+      const key = work.image.split('https://herfa.s3.eu-west-2.amazonaws.com/')[1];
+      await deleteImage(key);
+    }
   } catch (error) {
-    console.log(error);
     if (error.name === 'ValidationError') {
       return next(customError(error.message, 400));
     }
--- server/utils/validation/workEditValidation.js
+++ server/utils/validation/workEditValidation.js
@@ -4,6 +4,9 @@ const workEditValidation = Joi.object({
   title: Joi.string().max(50).required(),
   content: Joi.string().required(),
   id: Joi.number().integer().positive().required(),
+  workImg: Joi.object().keys({
+    type: Joi.string().valid('image/jpeg', 'image/png').required(),
+  }).unknown(),
   image: Joi.string(),
 });
 
