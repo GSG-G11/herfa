b8c84ad38e4eaaa185dfcc43cc24c2ee9a2e8c73
unknown <stm1998@hotmail.com>
2022-05-11 18:08:29 +0300

    refactor: pass reviews data to Reviews Component as props

--- client/src/Components/Reviews/index.tsx
+++ client/src/Components/Reviews/index.tsx
@@ -1,27 +1,27 @@
-import React, { useContext } from 'react';
-import { Carousel } from 'antd';
+import React from 'react';
+import { Carousel, Empty } from 'antd';
 import { useTranslation } from 'react-i18next';
 import ReviewCard from './ReviewCard';
-import { TopTenReviews } from '../../utils';
-import { ServiceLocation } from '../../Context/ServiceLocationContext';
+import { TopTenReviews, HomeReviewProps } from '../../utils';
 import './style.css';
 
-function HomeReview() {
+function HomeReview({ data }: HomeReviewProps) {
   const { t } = useTranslation();
-  const { data: { topTenReviews } } = useContext(ServiceLocation);
   return (
-    <div className="reviewsSection">
-      <h2 className="headline-text">
-        {t('what-they-say-about-us')}
-      </h2>
-      <div className="carouselContainer">
-        <Carousel autoplay arrows className="reviewSlider">
-          {topTenReviews.map(
-            (item: TopTenReviews) => <ReviewCard key={item.userId} review={item} />,
-          )}
-        </Carousel>
+    data?.length ? (
+      <div className="reviewsSection">
+        <h2 className="headline-text">
+          {t('what-they-say-about-us')}
+        </h2>
+        <div className="carouselContainer">
+          <Carousel autoplay arrows className="reviewSlider">
+            {data?.map(
+              (item: TopTenReviews) => <ReviewCard key={item.userId} review={item} />,
+            )}
+          </Carousel>
+        </div>
       </div>
-    </div>
+    ) : (<Empty description={(<span>{t('no-data')}</span>)} />)
   );
 }
 
