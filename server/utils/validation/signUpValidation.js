027af2f30ad43563f90236ea8bb48d8e030b8344
Saleh T. Marouf <54906217+devstm@users.noreply.github.com>
2022-05-25 22:17:32 +0300

    fix: change password pattern

--- server/utils/validation/signUpValidation.js
+++ server/utils/validation/signUpValidation.js
@@ -4,7 +4,7 @@ const signUpValidation = Joi.object({
   first_name: Joi.string().required(),
   last_name: Joi.string().required(),
   email: Joi.string().email().required(),
-  password: Joi.string().pattern(/^[a-zA-Z0-9]{8,30}$/),
+  password: Joi.string().required(),
   phone: Joi.string().required(),
   whatsapp: Joi.string().required(),
   locationId: Joi.number().required(),
